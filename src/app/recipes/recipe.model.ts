891d2861c933b76526f59da9baea1b3aaf293570
HagenGaryP <hagengaryp@gmail.com>
2022-05-09 16:14:21 -0400

    added recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+// Recipe model for the data a recipe will hold
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
